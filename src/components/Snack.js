5e272a6b558aad84df1e57705fea5e9bdc70aa51
sfox123 <saravanagobi.20200692@iit.ac.lk>
2022-03-25 16:06:34 +0530

    component class 2

---
+++ src/components/Snack.js
@@ -0,0 +1,46 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { connect } from "react-redux";
+import { toggleSnack } from "../actions";
+
+import MuiAlert from "@mui/material/Alert";
+import Snackbar from "@mui/material/Snackbar";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const Snack = ({ openSnack, handleSnack }) => {
+  const { snackOpen, snackMessage } = openSnack;
+
+  const handleCloseSnack = () => {
+    const SNACK = {
+      snackOpen: false,
+      snackMessage: null,
+    };
+    handleSnack(SNACK);
+  };
+  return ReactDOM.createPortal(
+    <Snackbar
+      open={snackOpen}
+      autoHideDuration={6000}
+      onClose={handleCloseSnack}
+    >
+      <Alert
+        onClose={handleCloseSnack}
+        severity="success"
+        sx={{ width: "100%" }}
+      >
+        {snackMessage}
+      </Alert>
+    </Snackbar>,
+    document.querySelector("#snack")
+  );
+};
+
+const mapStateToProps = (state) => {
+  return state;
+};
+
+export default connect(mapStateToProps, { handleSnack: toggleSnack })(Snack);
